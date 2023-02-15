b65da6dd0e0b1bf546463e539b1483766cb83c7d
Ryan <80393948+MrRyanXu@users.noreply.github.com>
2023-02-15 18:33:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,25 @@ template_id = os.environ["TEMPLATE_ID"]
 
 def get_weather():
   url = "http://apis.juhe.cn/simpleWeather/query?key=fc2734eed55956fcb86ed3a43bd86212&city=" + city
-  res = requests.get(url).json()
-  weather = res['realtime']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  response = requests.get(url)
+  data = json.loads(response.text)
+  if data['error_code'] == 0:
+    city = data['city']
+    current_temp = data['realtime']['temperature']
+    current_humidity = data['realtime']['humidity']
+    current_weather = data['realtime']['info']
+    current_wind_direction = data['realtime']['direct']
+    current_wind_power = data['realtime']['power']
+    current_aqi = data['realtime']['aqi']
+
+    print(f"{city}在2023年2月15日的天气情况是：")
+    print(f"当前温度：{current_temp}°C")
+    print(f"当前湿度：{current_humidity}%")
+    print(f"当前天气：{current_weather}")
+    print(f"风向和风力：{current_wind_direction}，{current_wind_power}")
+    print(f"空气质量指数（AQI）：{current_aqi}")
+else:
+    print("数据获取失败。")
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
